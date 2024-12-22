82aa9e49ca66544fe8a26696a61e28879d4f5a60
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-12-22 17:04:10 -0300

    ✨Feat: Criando as operações CRUD de Doctors

--- api/src/repositories/doctor.repository.ts
+++ api/src/repositories/doctor.repository.ts
@@ -1,5 +1,28 @@
+import { db } from "@/lib/prisma";
+import { Doctor } from "@prisma/client";
+
 export class DoctorRepository {
   public static async getAll() {
+    return await db.doctor.findMany();
+  }
 
+  public static async create(doctor: Doctor) {
+    await db.doctor.create({ data: doctor });
+  }
+
+  public static async update(doctor: Doctor) {
+    await db.doctor.update({
+      data: doctor, where: {
+        id: doctor.id
+      }
+    });
+  }
+
+  public static async delete(id: number) {
+    await db.doctor.delete({
+      where: {
+        id
+      }
+    });
   }
 }
